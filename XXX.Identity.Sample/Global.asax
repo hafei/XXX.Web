5031e9d0703f3e1e476ac4605443ccc403752360
hafei <zhangmengcoding@gmail.com>
2018-05-01 16:37:07 +0800

    add identity sample

---
+++ XXX.Identity.Sample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="XXX.Identity.Sample.MvcApplication" Language="C#" %>
---
+++ XXX.Identity.Sample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(XXX.Identity.Sample.Startup))]
+namespace XXX.Identity.Sample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
